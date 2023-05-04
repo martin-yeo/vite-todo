35ad69863576a09a4712a60dbcd085dd38f9f006
martin-yeo <sangsooyeo@gmail.com>
2023-05-04 18:39:33 +0900

    app.jsx 간단히

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       목원대 투두 만들기
